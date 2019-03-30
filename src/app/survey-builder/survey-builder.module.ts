fb5e2c57d83cc50bf74d27720a05490a38400aef
chadcollins <chadrcollins91@gmail.com>
2019-03-30 09:49:06 -0400

    Created modules

---
+++ src/app/survey-builder/survey-builder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SurveyBuilderRoutingModule } from './survey-builder-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SurveyBuilderRoutingModule
+  ]
+})
+export class SurveyBuilderModule { }
