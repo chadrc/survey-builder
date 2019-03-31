77e70fa3a192e2672be127efd27dfff304919508
chadcollins <chadrcollins91@gmail.com>
2019-03-31 19:11:00 -0400

    Display of survey name and link

--- src/app/survey-builder/builder/builder.component.ts
+++ src/app/survey-builder/builder/builder.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SurveyBuilderService} from '../survey-builder.service';
 import {Survey} from '../../shared/models/survey';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-builder',
@@ -18,22 +19,46 @@ export class BuilderComponent implements OnInit {
   }
 
   private _surveys: Survey[] = [];
-
   get surveys(): Survey[] {
     return this._surveys;
   }
 
-  constructor(private surveyBuilderService: SurveyBuilderService) { }
+  private _selectedSurveyIndex: number;
+  get selectedSurvey() {
+    return this._surveys[this._selectedSurveyIndex];
+  }
+
+  get selectedSurveyLink() {
+    return this.selectedSurvey ? `${window.location.origin}/survey/${this.selectedSurvey.id}` : '';
+  }
+
+  constructor(
+    private surveyBuilderService: SurveyBuilderService,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      const id = params['id'];
+      this._selectedSurveyIndex = this.surveys.findIndex(survey => survey.id === id);
+    });
+
     this.surveyBuilderService.getSurveys().subscribe(surveys => {
       this._surveys = surveys;
     });
+
+    const initialId = this.activatedRoute.snapshot.params['id'];
+    this._selectedSurveyIndex = this.surveys.findIndex(survey => survey.id === initialId);
   }
 
   createSurvey() {
     this.surveyBuilderService.newSurvey().subscribe(survey => {
       this.surveys.push(survey);
     });
+  }
+
+  surveyLink(survey: Survey) {
+    return `/build/${survey.id}`;
   }
 }
--- src/app/survey-builder/survey-builder-routing.module.ts
+++ src/app/survey-builder/survey-builder-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {BuilderComponent} from './builder/builder.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: BuilderComponent
+  },
+  {
+    path: ':id',
     component: BuilderComponent
   }
 ];
