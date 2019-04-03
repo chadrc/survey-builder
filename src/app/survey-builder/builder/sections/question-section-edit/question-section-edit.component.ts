0d582fa0ef751cd7455b6a531ae5fc69c2785c24
chadcollins <chadrcollins91@gmail.com>
2019-04-02 20:21:27 -0400

    Hooked up adding answers and editing answer values

--- src/app/survey-builder/builder/sections/question-section-edit/question-section-edit.component.ts
+++ src/app/survey-builder/builder/sections/question-section-edit/question-section-edit.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {QuestionSection} from '../../../../shared/models/survey-sections/question-section';
 import {SurveyBuilderService} from '../../../survey-builder.service';
+import {QuestionSectionAnswer} from '../../../../shared/models/survey-sections/question-section-answer';
 
 @Component({
   selector: 'app-question-section-edit',
@@ -8,17 +9,48 @@ import {SurveyBuilderService} from '../../../survey-builder.service';
   styleUrls: ['./question-section-edit.component.scss']
 })
 export class QuestionSectionEditComponent implements OnInit {
+  private _answers: QuestionSectionAnswer[] = [];
+
+  get answers(): QuestionSectionAnswer[] {
+    return this._answers;
+  }
+
+  set answers(value: QuestionSectionAnswer[]) {
+    this._answers = value;
+  }
+
   @Input() surveyId: string;
   @Input() section: QuestionSection;
 
-  constructor(private surveyBuilderService: SurveyBuilderService) { }
+  constructor(private surveyBuilderService: SurveyBuilderService) {
+  }
 
   ngOnInit() {
+    this.answers = this.section.answers;
   }
 
   editSectionField<K extends keyof QuestionSection>(field: K, value: QuestionSection[K]) {
     this.surveyBuilderService.editQuestionSection(this.surveyId, this.section.id, field, value).subscribe(modifiedSurvey => {
       console.log('modified survey', modifiedSurvey);
+    });
+  }
+
+  editAnswerField<K extends keyof QuestionSectionAnswer>(answerId: string, field: K, value: QuestionSectionAnswer[K]) {
+    this.surveyBuilderService.editQuestionSectionAnswer(
+      this.surveyId,
+      this.section.id,
+      answerId,
+      field,
+      value
+    ).subscribe(modifiedSurvey => {
+      console.log('modified survey', modifiedSurvey);
+    });
+  }
+
+  addAnswer() {
+    this.surveyBuilderService.addAnswerToQuestionSection(this.surveyId, this.section.id).subscribe(modifiedSurvey => {
+      this.answers = (modifiedSurvey.sections
+        .find(s => s.id === this.section.id) as QuestionSection).answers;
     });
   }
 }
