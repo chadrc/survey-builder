13845b39f8f5afaf6ca2adf5146674d7fa5d6219
chadcollins <chadrcollins91@gmail.com>
2019-04-06 10:38:44 -0400

    Removed integration test from section panels

--- src/app/survey-builder/builder/section-panels/section-panels.component.spec.ts
+++ src/app/survey-builder/builder/section-panels/section-panels.component.spec.ts
@@ -1,22 +1,10 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {SectionPanelsComponent} from './section-panels.component';
 
 describe('SectionPanelsComponent', () => {
   let component: SectionPanelsComponent;
-  let fixture: ComponentFixture<SectionPanelsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SectionPanelsComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SectionPanelsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new SectionPanelsComponent();
   });
 
   it('should create', () => {
