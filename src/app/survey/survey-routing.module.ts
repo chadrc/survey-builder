ded1d6efcbd77e5e647050e387bdef49988fdd78
chadcollins <chadrcollins91@gmail.com>
2019-04-06 11:45:03 -0400

    Survey component now takes a parameter

--- src/app/survey/survey-routing.module.ts
+++ src/app/survey/survey-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TakeComponent} from './take/take.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: ':id',
     component: TakeComponent
   }
 ];
