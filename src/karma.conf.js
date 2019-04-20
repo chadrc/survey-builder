223750ec300d528c2f6c49d9e9015d5aaec02d26
chadcollins <chadrcollins91@gmail.com>
2019-04-20 15:28:00 -0400

    Setup report outputs from tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,5 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
+
+const reportPath = path.resolve(__dirname, '../dist/test-reports');
 
 module.exports = function (config) {
   config.set({
@@ -8,6 +11,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-htmlfile-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -21,17 +25,28 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    htmlReporter: {
+      outputFile: `${reportPath}/unit/index.html`,
+
+      // Optional
+      pageTitle: 'Unit Tests',
+      subPageTitle: 'Survey Builder Unit Tests',
+      groupSuites: true,
+      useCompactStyle: true,
+      useLegacyStyle: true,
+      showOnlyFailed: false
+    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/survey-app'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: `${reportPath}/coverage`,
+      reports: ['html', 'lcovonly', 'json-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'html', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['CIChrome'],
     singleRun: true
   });
 };
